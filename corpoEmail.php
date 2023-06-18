3f683601080f520873b15fd08398b402b78b68b0
João Victor Souza <jvsouzasantos@id.uff.br>
2023-06-18 18:34:33 -0300

    feat: centralizando corpo do email

--- corpoEmail.php
+++ corpoEmail.php
@@ -7,9 +7,17 @@ function corpoEmail($nome, $email, $mensagem) {
             body {
                 font-family: Arial, sans-serif;
                 background-color: #f9f9f9;
-                max-width: 70%;
+            }
+            .container {
+                max-width: 600px;
+                margin: 0 auto;
+                padding: 20px;
+                background-color: #ffffff;
+                border-radius: 5px;
+                box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
             }
             h1 {
+                color: #ff0000;
                 font-size: 24px;
                 margin-bottom: 30px;
             }
@@ -26,10 +34,12 @@ function corpoEmail($nome, $email, $mensagem) {
         </style>
     </head>
     <body>
-        <h1>Informações do formulário<h1>
-        <h2>Nome do cliente: ' . $nome . '</h2>
-        <h2>Email: ' . $email . '</h2>
-        <p>' . $mensagem . '</p>
+        <div class="container">
+            <h1>Informações do formulário<h1>
+            <h2>Nome do cliente: ' . $nome . '</h2>
+            <h2>Email: ' . $email . '</h2>
+            <p>' . $mensagem . '</p>
+        </div>
     </body>
     </html>';
 
